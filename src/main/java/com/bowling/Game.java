805ec57bcb325dad6b90ca99c6de501f84455e72
mayurvpatil <mayurvpatil@live.com>
2021-03-30 12:08:49 +0530

    total score fix

--- src/main/java/com/bowling/Game.java
+++ src/main/java/com/bowling/Game.java
@@ -71,50 +71,62 @@ public class Game {
 		Frame secondLastFrame = null;
 		// Update spare - previous frame.
 
-		if ((frame - 1) >= 0) {
-			lastFrame = gameData.get(players[currentPlayer]).get(frame - 1);
+		int lastFrameNumber = frame - 1;
+		if (lastFrameNumber >= 0) {
+			lastFrame = gameData.get(players[currentPlayer]).get(lastFrameNumber);
 			// Check if last frame strike and next frame is open frame.
 			if (lastFrame.getFirst() == 10) {
 				if (currentFirst != -1 && currentSecond != -1) {
-					lastFrame.setFrameScore(currentFirst + currentSecond);
+					lastFrame.setFrameScore(10 + currentFirst + currentSecond);
+					if (lastFrameNumber == 0)
+						lastFrame.setTotalScore(10 + currentFirst + currentSecond);
 				}
 				// last frame is spare then add current first throw score.
 			} else if (lastFrame.getSecond() == 10 || (lastFrame.getFirst() + lastFrame.getSecond()) == 10) {
 				lastFrame.setFrameScore(10 + currentFirst);
 				// Total score needs to be updated
 			}
+		}
 
-			// Update spike - second last frame.
-			if (frame >= 2) {
-				secondLastFrame = gameData.get(players[currentPlayer]).get(frame - 2);
-				if (secondLastFrame.getFirst() == 10) {
+		// Update spike - second last frame.
+		if (frame >= 2) {
+			secondLastFrame = gameData.get(players[currentPlayer]).get(frame - 2);
+			if (secondLastFrame.getFirst() == 10) {
 
-					int frameCount = 10;
-					if (lastFrame.getFirst() == 10)
-						frameCount += (10 + currentFirst);
-					else if (lastFrame.getSecond() == 10 || (lastFrame.getFirst() + lastFrame.getSecond() == 10))
-						frameCount += 10;
+				int frameCount = 10;
+				if (lastFrame.getFirst() == 10)
+					frameCount += (10 + currentFirst);
+				else if (lastFrame.getSecond() == 10 || (lastFrame.getFirst() + lastFrame.getSecond() == 10))
+					frameCount += 10;
 
-					// Third condition handled in spare udpade.
+				// Third condition handled in spare udpade.
 
-					secondLastFrame.setFrameScore(frameCount);
-
-				}
+				secondLastFrame.setFrameScore(frameCount);
 
 			}
 
-			/*
-			 * int totalScore = 0; if((frame >= 3)) { totalScore =
-			 * gameData.get(players[currentPlayer]).get(frame - 3).getTotalScore(); } else {
-			 * totalScore = 0; }
-			 * 
-			 * if(secondLastFrame != null) secondLastFrame.setTotalScore(totalScore +
-			 * secondLastFrame.getFrameScore());
-			 * 
-			 * if(lastFrame.getFrameScore() != -1) {
-			 * lastFrame.setTotalScore(secondLastFrame.getTotalScore() +
-			 * lastFrame.getFrameScore()); }
-			 */
+		}
+
+		udpateTotalScore();
+
+	}
+
+	private void udpateTotalScore() {
+
+		for (Map.Entry<String, List<Frame>> entry : gameData.entrySet()) {
+			int totalScore = 0;
+			for (Frame frame : entry.getValue()) {
+				if (frame.getTotalScore() == -1) {
+					if (frame.getFrameScore() != -1) {
+						totalScore += frame.getFrameScore();
+						frame.setTotalScore(totalScore);
+					} else {
+						return;
+					}
+				} else {
+					totalScore = frame.getTotalScore();
+				}
+			}
 		}
 
 	}
